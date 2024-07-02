47858914b723c93ca195da400b0413e1a53048ec
rasty <hvozdetskiyyrii@gmail.com>
2024-07-02 16:21:32 +0300

    models

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
+
+    # Many to one, один-до-багатьох (в одного user може бути багато замовленнь)
+    # on_delete=CASCADE означає, що якщо User видаляється, всі пов'язані з ним SalesOrder також будуть видалені.
+    # null=True якщо раніше були створені замовлення(моделі), то їм присвоюється null замість user
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
