bca9cde23f73eabfbfa5c24b2131f3602ed1c9ed
rasty <hvozdetskiyyrii@gmail.com>
2024-07-11 20:04:59 +0300

    update

--- orders/models.py
+++ orders/models.py
@@ -9,7 +9,7 @@ class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
 
-    # Many to one, один-до-багатьох (в одного user може бути багато замовленнь)
+    # Many to one, багато до одного (в одного user може бути багато замовленнь)
     # on_delete=CASCADE означає, що якщо User видаляється, всі пов'язані з ним SalesOrder також будуть видалені.
     # null=True якщо раніше були створені замовлення(моделі), то їм присвоюється null замість user
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
